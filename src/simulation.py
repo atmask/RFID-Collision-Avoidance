a2ed6849ab63917afaf78f0ace092d5d50bafb2a
atmask <benjaminajm@gmail.com>
2023-03-07 22:24:56 -0500

    more work

--- src/simulation.py
+++ src/simulation.py
@@ -1,6 +1,7 @@
 
 from tag import BaseTag
 import time
+import settings
 
 def run_simulation(num_tags=10, tag_cls=BaseTag):
     # Create a collection fo tags
@@ -8,7 +9,16 @@ def run_simulation(num_tags=10, tag_cls=BaseTag):
 
     #Current epoch time
     min_time = int(time.time())
+    max_time=min_time + settings.SIMULATED_DURATION
 
     for t in tags:
-        print(f"TIME: {t.generate_time(min_time)}, tag: {t.id}")
+        print(f"TIME: {t.generate_time(min_time, max_time)}, tag: {t.id}")
+
+
+    # for each timestamp in the SIMULATED_DURATION
+        # check if multiple tags attempted to transmit
+        # if yes:
+            # collision detection
+        # else:
+            # continue
 
