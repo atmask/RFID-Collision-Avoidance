9c9a7208643b6284485a3bff1769f8791b5c8a93
atmask <benjaminajm@gmail.com>
2023-03-16 22:31:00 -0400

    Add more to collision work

--- src/reader.py
+++ src/reader.py
@@ -14,17 +14,43 @@ class BinaryTagReader(BaseTagReader):
 
     def manage_collision(self, tags):
         super().manage_collision()
-        self.binary_search(tags)
+        return self.binary_search(tags)
 
-    def _increment_bit_string(self, bitstr):
-        return bin(int(bitstr, 2)+1)
+    def _get_bit_string(self, i):
+        bit_arr = ['0b000', '0b001', '0b010', '0b011', '0b100', '0b101', '0b110', '0b111']
+        if i > 7:
+            return
+        else:
+            return bit_arr(i)
     
-    def binary_search(self, tags, id=None):
+    def binary_search(self, tags, id=''):
         '''Transmit an ID with three numbers. If collision then manage_collision with subset'''
         if len(tags) <= 1:
-            return tags
+            print(f'de-collided: {tags}')
+            return len(tags)
         
-        id = "0" if not id else self._increment_bit_string(id)
+        if id != '':
+            # Then find matching tags
+            matching_tags = [t for t in tags if t.id.startswith(id)]
+        else:
+            matching_tags = tags
+
+        # Need all permuations of three bits 000 001 010 011 100 101 110 111
+        if matching_tags > 1:
+            for i in range(8):
+                tmp_id=f'{id}{self._get_bit_string(i)}'
+
+                # maybe 1/8 depending on how slots are caluclated
+                return 1 + self.binary_search(matching_tags,tmp_id)
+
+
+
+
+
+
+        
+        
+
 
 
 
